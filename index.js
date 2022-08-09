8bf626e29eaeb16ad2df99b9c591928a0efcd391
Pedro <pedro.chn01@gmail.com>
2022-08-09 12:16:27 -0700

    starting

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-class Stack {}
+class Stack {
+    
+}
 
-class Queue {}
+class Queue {
+    #list
+
+    constructor(list) {
+        this.#list = list || [];
+    }
+    enqueue(item) { 
+        this.#list.push(item);
+    }
+
+    dequeue() {
+        return this.#list.splice(0, 1);
+    }
+
+    get count() {
+        return this.list.length;
+    }
+    get next() {
+        return this.#list[0];
+    }
+}
+const Queue = new Numbers();
+
 
 module.exports = { Stack, Queue };
--- index.test.js
+++ index.test.js
@@ -6,4 +6,24 @@ describe("Stack Class", () => {
     expect(stack.peek()).toEqual(1);
   });
   // add more tests here...
+  describe("Queue Class", () => {
+    it("#enqueue should add an item to the back", () => {
+      const queue = new Queue();
+      queue.enqueue("fox");
+      queue.enqueue("goose");
+      expect(queue.count).toBe(2);
+      expect(queue.next).toBe("fox");
+    });
+  });
+  describe("Queue Class", () => {
+    it("#dequeue should add an item to the back", () => {
+      const queue = new Queue();
+      queue.enqueue("fox");
+      queue.enqueue("goose");
+      const item = queue.dequeue();
+      expect(queue.count).toBe(1);
+      expect(queue.next).toBe("goose");
+      expect(item).toBe("fox")
+    });
+  });
 });
